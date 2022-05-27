acd00fa46f9ed0480490ca8998453a5654405d6b
UtsavSomaiya <utsavsomaiya4464@gmail.com>
2022-05-27 15:30:32 +0530

    Changes name => username & use min file

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="{{ asset('icon/retail-store-icon-18.png') }}" type="image/x-icon">
         <title>Retail Shop(Admin)</title>
-        <link rel="stylesheet" href="{{ asset('css/alertify.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/alertify.min.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
@@ -104,7 +104,7 @@
                 </div>
             </div>
         </div>
-        <script src="{{ asset('js/alertify.js') }}"></script>
+        <script src="{{ asset('js/alertify.min.js') }}"></script>
         @if(session('success'))
             <script>
                 alertify.success("{{ session('success') }}");
